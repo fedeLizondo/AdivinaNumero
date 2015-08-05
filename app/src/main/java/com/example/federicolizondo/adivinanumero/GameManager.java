46f0b5d4fbb61f2ff61d9443b5fb1a0966eb4996
fedeLizondo <federicoalizondo@gmail.com>
2015-08-04 23:16:23 -0300

    Se Agrego Clase GameManager.java y Numero.java

---
+++ app/src/main/java/com/example/federicolizondo/adivinanumero/GameManager.java
@@ -0,0 +1,105 @@
+package com.example.federicolizondo.adivinanumero;
+
+import java.util.ArrayList;
+
+/**
+ * Created by federicolizondo on 04/08/15.
+ * Esta clase se encarga de administrar el juego
+ */
+public class GameManager {
+
+    private ArrayList<Numero> lNumeros;
+
+    private int unidadMil;
+    private int centena;
+    private int decena;
+    private int unidad;
+    private boolean win;
+
+    public GameManager() {
+        this.lNumeros = new ArrayList<Numero>();
+        int[] a = convertirAUnidades(nroRandom());
+        this.unidadMil = a[0];
+        this.centena = a[1];
+        this.decena = a[2];
+        this.unidad = a[3];
+        this.win = false;
+    }
+
+    //Metodos publicos
+    public void intento(Numero n) {
+        n.verificar(unidadMil, centena, decena, unidad);
+        this.win = n.sosElNumero(unidadMil, centena, decena, unidad);
+        lNumeros.add(n);
+    }
+
+    public boolean estaIngresado(int unidadMil, int centena, int decena, int unidad) {
+        int size = lNumeros.size(),
+                cont = 0;
+        while (cont < size && lNumeros.get(cont).sosElNumero(unidadMil, centena, decena, unidad)) {
+            cont++;
+        }
+        return (cont < size);
+    }
+    //-----Getters and Setters
+
+    public int darNumero() {
+        return convertirANumero(this.unidadMil, this.centena, this.decena, this.unidad);
+    }
+
+    public boolean Gane() {
+        return win;
+    }
+
+
+    //METODOS PRIVADOS
+
+    private int nroRandom() {
+        int um = 0,
+                c = 0,
+                d = 0,
+                u = 0,
+                aux = 0;
+        while (aux < 1024) {
+            while (um == 0) {
+                um = (int) ((Math.random() * 10) % 10);
+            }
+            do {
+                c = (int) ((Math.random() * 10) % 10);
+            } while (c == um);
+            do {
+                d = (int) ((Math.random() * 10) % 10);
+            } while (d == c || d == um);
+            do {
+                u = (int) ((Math.random() * 10) % 10);
+            } while (u == um || u == c || u == d);
+        }
+        return aux;
+    }
+
+    //METODOS STATICOS
+    public static int convertirANumero(int unidadMil, int centena, int decena, int unidad) {
+        return unidadMil * 1000 + centena * 100 + decena * 10 + unidad;
+    }
+
+    public static int[] convertirAUnidades(int nro) {
+        int[] aNro = new int[3];
+        //unidad
+        aNro[3] = nro % 10;
+        //decena
+        aNro[2] = (nro % 100 - aNro[3]) / 10;
+        //centena
+        aNro[1] = (nro % 1000 - (aNro[2] * 10 + aNro[3])) / 100;
+        //unidad de Mil
+        aNro[0] = (nro - aNro[1] * 100 + aNro[2] * 10 + aNro[3]) / 1000;
+        return aNro;
+    }
+
+
+/*  public void guardarEnBD()
+    {
+        Guardaria el estado actual para recuperar en caso de salir
+    }
+*/
+
+}
---
+++ app/src/main/java/com/example/federicolizondo/adivinanumero/Numero.java
@@ -0,0 +1,79 @@
+package com.example.federicolizondo.adivinanumero;
+
+/**
+ * Created by federicolizondo on 04/08/15.
+ * Es una clase que contiene todos los datos del numero ingresado;
+ */
+public class Numero {
+
+    private int unidad;
+    private int decena;
+    private int centena;
+    private int uMil;//Unidad de Mil
+
+    private short bien;
+    private short regular;
+
+    public Numero(int unidadDeMil, int centena, int decena, int unidad) {
+        this.uMil = unidadDeMil;
+        this.centena = centena;
+        this.decena = decena;
+        this.unidad = unidad;
+
+        this.bien = 0;
+        this.regular = 0;
+
+    }
+
+    public void verificar(int vUnidadDeMil, int vCentena, int vDecena, int vUnidad) {
+        /*
+        Esta función compara una unidad contra las pasadas
+        modifica la cantidad de  BIEN y las REGULARES
+        */
+        if (uMil == vUnidadDeMil)
+            bien++;
+        else if (uMil == vCentena || uMil == vDecena || uMil == vUnidad)
+            regular++;
+        /////////////////////////////////////////////////////////////
+        if (centena == vCentena)
+            bien++;
+        else if (centena == vUnidadDeMil || centena == vDecena || centena == vUnidad)
+            regular++;
+        ////////////////////////////////////////////////////////////
+        if (decena == vDecena)
+            bien++;
+        else if (decena == vUnidad || decena == vCentena || decena == vUnidad)
+            regular++;
+        ////////////////////////////////////////////////////////////
+        if (unidad == vUnidad)
+            bien++;
+        else if (unidad == vUnidadDeMil || unidad == vCentena || unidad == vDecena)
+            regular++;
+    }
+
+    public int darCantidadBien() {
+        return bien;
+    }
+
+    public int darCantidadRegular() {
+        return regular;
+    }
+
+    public int darNro() {
+        return uMil * 1000 + centena * 100 + decena * 10 + unidad;
+    }
+
+    public boolean sosElNumero(int vUnidadDeMil, int vCentena, int vDecena, int vUnidad) {
+        return uMil == vUnidadDeMil && centena == vCentena && decena == vDecena && unidad == vUnidad;
+    }
+
+    public String darInfo() {
+        return "Numero: " + this.darNro() + "\nCantidad Bien: " + bien + "\nCantidad Regular:" + regular;
+    }
+
+    @Override
+    public String toString() {
+        //return super.toString();
+        return this.darNro() + ":  " + bien + " bien  " + regular + " regular.";
+    }
+}
